f9fa90c990e7fed2f9098e78fdec9913eac126b8
Robert Sandfield <robert.f.sandfield@gmail.com>
2020-05-02 14:44:11 -0700

    Characters page now remembers context and loads display from it. Clickable rows are not yet working.

--- beastidex.js
+++ beastidex.js
@@ -31,6 +31,33 @@ app.get('/monsters',function(req,res,next){
 
 app.get('/characters',function(req,res,next){
   var context = {};
+  context.characters = [
+    { characterID:1, name:"Robert Sandfield", race:"Human",
+      class:"Wizard", level:5, party:"Database Comrades",
+      strength:20, dexterity:20, constitution:20,
+      intelligence:20, wisdom:20, charisma:20 },
+    { characterID:2, name:"Salah Salamey", race:"Dinosaur",
+      class:"Monk", level:5, party:"Database Comrades",
+      strength:20, dexterity:20, constitution:20,
+      intelligence:20, wisdom:20, charisma:20 },
+    { characterID:3, name:"Jeff O'Saur", race:"Dinosaur",
+      class:"Bard", level:3, party:"Derpestarians",
+      strength:13, dexterity:8, constitution:15,
+      intelligence:11, wisdom:8, charisma:20 },
+    { characterID:4, name:"Bill", race:"Donkey",
+      class:"Bourgeoisie", level:8, party:"Derpestarians",
+      strength:14, dexterity:8, constitution:15,
+      intelligence:7, wisdom:6, charisma:8 },
+    { characterID:5, name:"Shrek", race:"Ogre",
+      class:"Fighter", level:6, party:"Shronkey",
+      strength:18, dexterity:11, constitution:16,
+      intelligence:12, wisdom:9, charisma:8},
+    { characterID:6, name:"Bard", race:"Donkey",
+      class:"Bourgeoisie", level:8, party:"Shronkey",
+      strength:14, dexterity:8, constitution:15,
+      intelligence:11, wisdom:9, charisma:18 }
+  ];
+
   res.render('characters',context);
 });
 
--- public/js/character.js
+++ public/js/character.js
@@ -1,19 +1,9 @@
 window.addEventListener("DOMContentLoaded", event => {
-    var jeff = {"characterID":1, "name":"Jeff O'Saur", "race":"Dinosaur",
-    "class":"Bard", "level":3, "party":"Derpestarians",
-    "strength":13, "dexterity":8, "constitution":15,
-    "intelligence":11, "widsom":8, "charisma":20};
-
     var table = document.getElementById("characters");
-    var row = document.createElement("div");
-    table.appendChild(row);
-    row.innerHTML="{{> characterRow jeff}}"
 });
 
-var characterList = {
-    "1":{}
-};
-
 function loadCharacter(button) {
-    
+    var id = button.getAttribute("id");
+    var character = context.find(element => element.characterID = id);
+    console.log(character);
 }
