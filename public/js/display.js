fd13a35dca5b16f8733e6ee77600f9278a726ef6
Robert Sandfield <robert.f.sandfield@gmail.com>
2020-05-04 06:56:56 -0700

    Experiment with alternate format

--- public/js/display.js
+++ public/js/display.js
@@ -11,3 +11,55 @@ function updateField(name, value) {
     document.getElementById(name + "-display").innerHTML = value;
     document.getElementById(name).value = value;
 }
+
+function addElement(form, elementID) {
+    form = document.getElementById(form);
+    
+    var id = Number.parseInt(context.reduce(function(prev, next){
+        return (prev[elementID] > next[elementID] ? prev[elementID] : next[elementID]);
+    }));
+    id += 1;
+
+    var element = {};
+    element[elementID] = id;
+
+    for(var i = 1; i < form.length-3; i++) {
+        var value = form.elements[i].value;
+        if(Number.parseInt(value)) {
+            value = Number.parseInt(value);
+        }
+        element[form.elements[i].id] = value;
+    }
+    
+    context.push(element);
+    addCharacterRow(element);
+}
+
+function modifyElement(elementID) {
+
+}
+
+function deleteElement(elementID) {
+
+}
+
+function addCharacterRow(element) {
+    var table = document.getElementById('characters');
+    var row = addNode(table, "tr", "")
+    row.setAttribute("class", "clickable-row");
+    row.setAttribute("id", element["characterID"]);
+    row.setAttribute("onclick", "loadElement(this, 'characterID')");
+
+    addNode(row, "td", element["name"]);
+    addNode(row, "td", element["race"]);
+    addNode(row, "td", element["class"]);
+    addNode(row, "td", element["level"]);
+    addNode(row, "td", element["party"]);
+}
+
+function addNode(parent, type, content) {
+    var node = document.createElement(type);
+    parent.appendChild(node);
+    node.innerHTML = content;
+    return node;
+}
