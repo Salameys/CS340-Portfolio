2111e9d99268c18bc487ea5fe722587a5cbc1f30
Salameys <59760507+Salameys@users.noreply.github.com>
2020-05-17 00:40:02 -0400

    Database fully operational with static values

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,4 +1,8 @@
 
+
+DROP TABLE IF EXISTS `Monster_Biome`;
+DROP TABLE IF EXISTS `Monster_Ability`;
+DROP TABLE IF EXISTS `Character_Monster`;
 DROP TABLE IF EXISTS `Monsters`;
 DROP TABLE IF EXISTS `Characters`;
 DROP TABLE IF EXISTS `Abilities`;
@@ -27,13 +31,6 @@ CREATE TABLE `Monsters` (
 PRIMARY KEY (`monsterID`)
 ) ENGINE=InnoDB;
 
-INSERT INTO `Monsters` (`name`, `description`, `size`, `type`, 
-`alignment`, `armor_class`, `hit_dice`, `speed`, 
-`fly_speed`, `strength`, `dexterity`, `constitution`,
-`intelligence`, `wisdom`, `charisma`, `challenge`, `source_book`) 
-VALUES ("Ancient White Dragon", "Terrifying beast of old", "Gargantuan", "Dragon", "CE",
-"20", "18d20", "40ft", "80ft", "26", "10", "26", "10", "13", "14", "20", "MM");
-
 CREATE TABLE `Characters` (
 `characterID` int(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(255) NOT NULL,
@@ -46,15 +43,9 @@ CREATE TABLE `Characters` (
 `intelligence` int NOT NULL,
 `wisdom` int NOT NULL,
 `charisma` int NOT NULL,
+`partyID` int,
  PRIMARY KEY (`characterID`)
 ) ENGINE=InnoDB;
-
-INSERT INTO `Characters` (`name`, `race`, `level`, `class`, `strength`,
-`dexterity`, `constitution`, `intelligence`, `wisdom`, `charisma`)
-VALUES ('Salah Salamey', 'Human', '20', 'Monk', '20', '20', '20',
-'20', '20', '20'), ('Robert Sandfield', 'Human', '20', 'Wizard', '20', '20', '20',
-'20', '20', '20');
-
 
 CREATE TABLE `Abilities` (
 `abilityID` int(11) NOT NULL AUTO_INCREMENT,
@@ -64,9 +55,6 @@ CREATE TABLE `Abilities` (
 `damage_dice` varchar(255) NOT NULL,
 PRIMARY KEY (`abilityID`)
 ) ENGINE=InnoDB;
-
-INSERT INTO `Abilities` (`name`, `damage_type`, `range`, `damage_dice`)
-VALUES ('Claw', 'Slashing', 'Melee', '2d4');
  
 CREATE TABLE `Biomes` (
 `biomeID` int(11) NOT NULL AUTO_INCREMENT,
@@ -75,19 +63,12 @@ CREATE TABLE `Biomes` (
 PRIMARY KEY (`biomeID`)
 ) ENGINE=InnoDB;
 
-INSERT INTO `Biomes`(`name`, `description`)
-VALUES('Desert', "I hate that sand, it's coarse and it gets everywhere");
-
-
 CREATE TABLE `Parties` (
 `partyID` int(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(255) NOT NULL,
 PRIMARY KEY (`partyID`)
 ) ENGINE=InnoDB;
 
-INSERT INTO `Parties`(`name`) VALUES ('Database Comrades');
-
-DROP TABLE IF EXISTS `Monster_Biome`;
 CREATE TABLE `Monster_Biome` (
 `monsterID` INT NOT NULL,
 `biomeID` INT NOT NULL,
@@ -99,7 +80,6 @@ FOREIGN KEY fk_biome(`biomeID`)
 REFERENCES `Biomes`(`biomeID`)
 ON DELETE CASCADE);
 
-DROP TABLE IF EXISTS `Monster_Ability`;
 CREATE TABLE `Monster_Ability` (
 `monsterID` INT NOT NULL,
 `abilityID` INT NOT NULL,
@@ -111,7 +91,6 @@ FOREIGN KEY `fk_ability`(`abilityID`)
 REFERENCES `Abilities`(`abilityID`)
 ON DELETE CASCADE);
 
-DROP TABLE IF EXISTS `Character_Monster`;
 CREATE TABLE `Character_Monster` (
 `characterID` INT NOT NULL,
 `monsterID` INT NOT NULL,
@@ -122,6 +101,44 @@ ON DELETE CASCADE,
 FOREIGN KEY `fk_monster3`(`monsterID`)
 REFERENCES `Monsters`(`monsterID`)
 ON DELETE CASCADE);
+
+
+ALTER TABLE `Characters`
+ADD FOREIGN KEY `partyID` (`partyID`) 
+REFERENCES `Parties` (`partyID`);
+
+
+INSERT INTO `Parties`(`name`) VALUES ('Database Comrades');
+
+INSERT INTO `Monsters` (`name`, `description`, `size`, `type`, 
+`alignment`, `armor_class`, `hit_dice`, `speed`, 
+`fly_speed`, `strength`, `dexterity`, `constitution`,
+`intelligence`, `wisdom`, `charisma`, `challenge`, `source_book`) 
+VALUES ("Ancient White Dragon", "Terrifying beast of old", "Gargantuan", "Dragon", "CE", "20", "18d20", "40", "80", "26", "10", "26", "10", "13", "14", "20", "MM"),
+('Cat', 'Meow', 'Tiny', 'Beast', 'CN', '12', '1d4', '40', '0', '3', '15', '10', '3','12', '7', '0', 'PHB');
+
+INSERT INTO `Characters` (`name`, `race`, `level`, `class`, `strength`,
+`dexterity`, `constitution`, `intelligence`, `wisdom`, `charisma`, `partyID`)
+VALUES ('Salah Salamey', 'Human', '20', 'Monk', '20', '20', '20', '20', '20', '20', 1), 
+('Robert Sandfield', 'Human', '20', 'Wizard', '20', '20', '20', '20', '20', '20', 1), 
+('Jeffosaurus', 'Dinosaur', '20', 'Barbarian', '20', '20', '20', '20', '20', '20', NULL);
+
+INSERT INTO `Abilities` (`name`, `damage_type`, `range`, `damage_dice`)
+VALUES ('Claw', 'Slashing', 'Melee', '2d4'), ('Bite', 'Piercing', 'Melee', '2d6');
+
+INSERT INTO `Biomes`(`name`, `description`)
+VALUES('Desert', "I hate that sand, it's coarse and it gets everywhere"), 
+ ('Frozen Wastes', "It's cold, and full of waste!");
+
+INSERT INTO `Monster_Biome` (`monsterID`, `biomeID`) 
+VALUES (1,2), (2,1), (2,2);
+
+INSERT INTO `Monster_Ability` (`monsterID`, `abilityID`)
+VALUES (1,1), (1,2), (2,1);
+
+INSERT INTO `Character_Monster` (`characterID`, `monsterID`)
+VALUES (1,1), (1,2), (2,1), (2,2);
+
 
 
 
